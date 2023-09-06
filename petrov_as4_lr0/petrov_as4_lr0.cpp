a5aa180522de95b5b226cab419c20064f115b25f
s1ash <s1ashpetrov@yandex.ru>
2023-09-07 00:36:43 +0300

    hello world и ввод символов

--- petrov_as4_lr0/petrov_as4_lr0.cpp
+++ petrov_as4_lr0/petrov_as4_lr0.cpp
@@ -1,1 +1,11 @@
-﻿
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+
+	std::cout << "vvedite tekst ";
+	std::string stroka;
+	std::getline(std::cin, stroka);
+}
